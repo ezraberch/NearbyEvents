356d0fcced874184f9a39a29c7a90d47060874f9
Ezra Berch <ezra@ezras-imac.fios-router.home>
2018-12-31 12:28:55 -0500

    Initial Commit

---
+++ NearbyEvents/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  NearbyEvents
+//
+//  Created by Ezra Berch on 12/31/18.
+//  Copyright © 2018 Ezra Berch. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
